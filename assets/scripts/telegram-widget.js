ff94c02792a84337b45b36456b9ce1ae9dcd0b58
Dino-Scripts <86493204+KerreLProd@users.noreply.github.com>
2023-08-08 18:05:29 +0300

    Delete assets/scripts directory

--- assets/scripts/telegram-widget.js
+++
@@ -1,8 +0,0 @@
-function loginWithTelegram() {
-    var botUsername = '@arztune_bot';
-    var botToken = '5844104350:AAEkYk8EOhJMCkYKKlytqummkJ_WH5rkmMU';
-    var redirectUrl = 'https://ваш_сайт.com/redirect'; // URL, на который пользователь будет перенаправлен после авторизации
-
-    var widget = new TelegramLoginWidget(botUsername, botToken, redirectUrl);
-    widget.render('telegram-login', {size: 'large', requestAccess: true});
-}
